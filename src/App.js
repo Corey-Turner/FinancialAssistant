d7902eb68f4bc1549abc37a69d61c910c8e1abf3
Corey Turner <turnerc1510@my.uwstout.edu>
2020-10-20 12:43:52 -0500

    renaming component code to component.js instead of index.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import MortgageCalc from "./components/MortgageCalculator/index";
+import MortgageCalc from "./components/MortgageCalculator/component";
 import Nav from "./components/Nav/nav";
-import DebtManager from "./components/DebtManager/index";
-import BudgetManager from "./components/BudgetManager/index";
-import TaxEstimator from "./components/TaxEstimator/index";
-import Home from "./components/Home/index";
+import DebtManager from "./components/DebtManager/component";
+import BudgetManager from "./components/BudgetManager/component";
+import TaxEstimator from "./components/TaxEstimator/component";
+import Home from "./components/Home/component";
 
 export default function App() {
   return (
@@ -18,7 +18,7 @@ export default function App() {
           <Route path="/mortgage-calculator" component={MortgageCalc} />
           <Route path="/tax-estimator" component={TaxEstimator} />
           <Route path="/debt-manager" component={DebtManager} />
-          <Route path="/budget-manager" component={MortgageCalc} />
+          <Route path="/budget-manager" component={BudgetManager} />
           <Route path="/" component={Home} />
         </Switch>
       </div>
